e4f930b7ba6d52234011541f9491b09dcfa64e54
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-09-12 11:52:43 +0800

    Rebase from 'release/humble/etsi_its_msgs_utils'

--- include/etsi_its_msgs_utils/impl/cam/cam_ts_getters.h
+++ include/etsi_its_msgs_utils/impl/cam/cam_ts_getters.h
@@ -2,7 +2,7 @@
 =============================================================================
 MIT License
 
-Copyright (c) 2023 Institute for Automotive Engineering (ika), RWTH Aachen University
+Copyright (c) 2023-2024 Institute for Automotive Engineering (ika), RWTH Aachen University
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -25,14 +25,13 @@ SOFTWARE.
 */
 
 /**
- * @file impl/cdd/cdd_access.h
- * @brief Main CDD access implementation header
+ * @file impl/cam/cam_ts_getters.h
+ * @brief Getter functions for the ETSI ITS CAM (TS)
  */
 
 #pragma once
 
-#include <cmath>
-
-#include <etsi_its_msgs_utils/impl/cdd/cdd_checks.h>
-#include <etsi_its_msgs_utils/impl/cdd/cdd_getters.h>
-#include <etsi_its_msgs_utils/impl/cdd/cdd_setters.h>
+namespace etsi_its_cam_ts_msgs::access {
+#include <etsi_its_msgs_utils/impl/cdd/cdd_v2-1-1_getters.h>
+#include <etsi_its_msgs_utils/impl/cam/cam_getters_common.h>
+} // namespace etsi_its_cam_ts_msgs::access
