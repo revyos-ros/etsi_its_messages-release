6b53c1b50498e37f3bf261248c5c5c549ed06739
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-09-25 16:12:52 +0800

    Rebase from 'upstream'

--- etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/cdd/cdd_checks.h
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/cdd/cdd_checks.h
@@ -2,7 +2,7 @@
 =============================================================================
 MIT License
 
-Copyright (c) 2023 Institute for Automotive Engineering (ika), RWTH Aachen University
+Copyright (c) 2023-2024 Institute for Automotive Engineering (ika), RWTH Aachen University
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -31,15 +31,7 @@ SOFTWARE.
 
 #pragma once
 
-namespace etsi_its_msgs {
-
-namespace cdd_access {
-
-  template <typename T1, typename T2>
-  void throwIfOutOfRange(const T1& val, const T2& min, const T2& max, const std::string val_desc) {
-    if (val < min || val > max) throw std::invalid_argument(val_desc+" value is out of range ("+std::to_string(min)+"..."+std::to_string(max)+")!");
-  }
-
-} // namespace cdd_access
-
-} // namespace etsi_its_msgs
+template <typename T1, typename T2>
+void throwIfOutOfRange(const T1& val, const T2& min, const T2& max, const std::string val_desc) {
+  if (val < min || val > max) throw std::invalid_argument(val_desc+" value is out of range ("+std::to_string(min)+"..."+std::to_string(max)+")!");
+}
